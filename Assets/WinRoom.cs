0a31dd6c2ae314ed671f35b30c82d22d39b37a73
Unknown <enoragon@gmail.com>
2018-02-15 14:14:04 -0600

    Trigger fireworks

--- Assets/WinRoom.cs
+++ Assets/WinRoom.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WinRoom : MonoBehaviour {
-	public Transform player;
+	public Transform spawnFireWorks;
+    public Transform FireworksPrefab;
 	// Use this for initialization
 
 	//TODO change this to be player
 	void OnTriggerEnter (Collider other) {
-		
+        Instantiate(FireworksPrefab, spawnFireWorks.position, spawnFireWorks.rotation);
 	}
 	
 
