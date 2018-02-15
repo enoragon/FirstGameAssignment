e716f1f2943d50a32b585a24b1ee02a112a5ea18
Unknown <enoragon@gmail.com>
2018-02-15 13:22:39 -0600

    Fireworks to the win area

---
+++ Assets/WinRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinRoom : MonoBehaviour {
+	public Transform player;
+	// Use this for initialization
+
+	//TODO change this to be player
+	void OnTriggerEnter (Collider other) {
+		
+	}
+	
+
+}
