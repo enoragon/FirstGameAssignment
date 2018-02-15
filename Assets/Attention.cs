26c5f6288c04284ad378d9f6c2abdfc0e9343461
Unknown <enoragon@gmail.com>
2018-02-15 11:52:48 -0600

    Setting the Scene

--- Assets/Attention.cs
+++ Assets/Attention.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class Attention : MonoBehaviour {
 	
 	public Transform player;
-	static Animator anim;
+    public Transform head;
+	Animator anim;
+
+    string state = "pace";
+	public GameObject[] waypoints;
+	int currentWaypoint = 0;
+	public float rotationSpeed = 0.6f;
+	public float walkSpeed = 1.5f;
+	public float runSpeed = 1.5f;
+	float WaypointPrecision = 5.0f;
+
+	public int walkDistance = 15;
+	public int runDistance = 10;
+	public int attackDistance = 5;
+    public int visionAngle = 30;
 
 	// Use this for initialization
 	void Start () {
@@ -14,20 +28,42 @@ public class Attention : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Vector3.Distance (player.position, this.transform.position) < 10) {
-			Vector3 direction = player.position - this.transform.position;
-			direction.y = 0;
+        Vector3 direction = player.position - this.transform.position;
+		direction.y = 0;
+        float angle = Vector3.Angle(direction, this.transform.forward);
+		if(state == "pace" && waypoints.Length > 0)
+		{
+			anim.SetBool("isIdle", false);
+			anim.SetBool("isWalking", true);
+			if(Vector3.Distance(waypoints[currentWaypoint].transform.position, transform.position) < WaypointPrecision)
+			{
+				currentWaypoint++;
+				if(currentWaypoint >= waypoints.Length){
+					currentWaypoint = 0;
+				}
 
-			anim.SetBool ("isIdle", false);
-			anim.SetBool ("isWalking", true);
-			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
-			if (direction.magnitude > 5) {
-				this.transform.Translate (0, 0, 0.05f);
+			}
 
-//			} else if (direction.magnitude > 5) { 
-//				anim.SetBool ("isWalking", false);
-//				anim.SetBool ("isRunning", true);
-//				this.transform.Translate (0, 0, 0.07f);
+			direction = waypoints [currentWaypoint].transform.position - transform.position;
+			this.transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction), rotationSpeed * Time.deltaTime);
+			this.transform.Translate (0, 0, Time.deltaTime * walkSpeed);
+		}
+		if (Vector3.Distance (player.position, this.transform.position) < walkDistance && (angle < visionAngle || state == "pursuing")) {
+			
+			state = "pursuing";			
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), rotationSpeed * Time.deltaTime);
+
+			if (direction.magnitude > runDistance) {
+				this.transform.Translate (0, 0, walkSpeed * Time.deltaTime);
+				anim.SetBool ("isAttacking", false);
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isWalking", true);
+
+            } else if (direction.magnitude > attackDistance) {
+				anim.SetBool ("isAttacking", false);
+				anim.SetBool ("isWalking", false);
+				anim.SetBool ("isRunning", true);
+				this.transform.Translate (0, 0, runSpeed * Time.deltaTime);
 			} else {
 				anim.SetBool ("isRunning", false);
 				anim.SetBool ("isAttacking", true);
@@ -35,8 +71,9 @@ public class Attention : MonoBehaviour {
 		} else {
 			anim.SetBool ("isAttacking", false);
 			anim.SetBool ("isRunning", false);
-			anim.SetBool ("isWalking", false);
-			anim.SetBool ("isIdle", true);
+			anim.SetBool ("isWalking", true);
+			state = "pace";
+
 		}
 	}
 }
