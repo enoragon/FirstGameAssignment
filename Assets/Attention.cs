d329539c435b90147e02d249320b8841172f5e04
Unknown <enoragon@gmail.com>
2018-02-14 23:19:31 -0600

    Adding spiders

---
+++ Assets/Attention.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attention : MonoBehaviour {
+	
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(player.position, this.transform.position) < 10)
+		{
+			Vector3 direction = player.position - this.transform.position;
+			direction.y = 0;
+
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
+			if(direction.magnitude > 5)
+			{
+				this.transform.Translate (0, 0, 0.05f);
+			}
+		}
+	}
+}
